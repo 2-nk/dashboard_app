d0dae72912639ae09922e50dfeb44b2f516863f9
2-nk <nklee624@gmail.com>
2025-06-30 11:24:13 +0900

    1step

---
+++ adidas_dashboard_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sms
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="Adidas US Sales Dashboard", layout="wide")
+st.title("👟 Adidas US Sales Data Dashboard")
